050234bff2110a8a3d668d725e6baad6e843bf84
Tirth Patel <tirthnp@live.com>
2019-06-30 00:43:27 -0230

    Updates

---
+++ precache-manifest.d107a03884dc4a387eebd9f85454a705.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480e2dda9c9f535bf1e5cb563f97dc2e",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "b85f51fc943c26de49e0",
+    "url": "/portfolio-react/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "119f922e6cf1355b24a8",
+    "url": "/portfolio-react/static/css/main.cfbdc2dc.chunk.css"
+  },
+  {
+    "revision": "b85f51fc943c26de49e0",
+    "url": "/portfolio-react/static/js/2.499aafb9.chunk.js"
+  },
+  {
+    "revision": "119f922e6cf1355b24a8",
+    "url": "/portfolio-react/static/js/main.4399f28e.chunk.js"
+  },
+  {
+    "revision": "11528ddda4154ce1e32b",
+    "url": "/portfolio-react/static/js/runtime~main.72b3206a.js"
+  },
+  {
+    "revision": "dafa1e9ffd814045a271dbc7fb5e6aae",
+    "url": "/portfolio-react/static/media/Tirth.dafa1e9f.JPG"
+  }
+]);
