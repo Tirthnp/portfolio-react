57a0b20cb42eabc83c1b88943f538f855d753fc7
Tirth Patel <tirthnp@live.com>
2019-07-01 22:28:54 -0230

    Filling education and experience

--- src/components/experience.js
+++ src/components/experience.js
@@ -2,17 +2,27 @@ import React,{Component} from 'react';
 import {Grid,Cell} from 'react-mdl';
 
 class Experience extends Component{
+    
     render(){
+        const des = this.props.jobDescription.map((des) =>
+    <li>{des}</li>
+  );
         return(
             <Grid>
                 <Cell col={4}>
-                    <p>{this.props.startYear}-{this.props.endYear}</p>
+                    <p>{this.props.startYear} - {this.props.endYear}</p>
                 </Cell>
                 <Cell col={8}>
                     <h4 style={{marginTop:'0px'}}>{this.props.jobName}</h4>
-                    <p>{this.props.jobDescription}</p>
+                    <h5>{this.props.companyName}</h5>
+                    <h6>{this.props.companyLocation}</h6>
+                    
+                    {des}
                 </Cell>
+                
             </Grid>
+            
+            
         )
     }
 }
