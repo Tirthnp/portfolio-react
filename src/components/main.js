f82e5e0d40819dd055710987620bbc5fd0ed5242
Tirth Patel <tirthnp@live.com>
2019-07-08 00:20:46 -0230

    changes

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch,Route} from 'react-router-dom';
+import {Switch,Route,Router} from 'react-router-dom';
 import Landing from './landingpage';
 import About from './aboutme';
 import Contact from './contact';
@@ -9,12 +9,13 @@ import Resume from './resume';
 
 const Main =()=>(
     <Switch>
-        
+        <Router basename={process.env.PUBLIC_URL}>
         <Route exact path="/" component={Landing} />
         <Route path="/aboutme" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="/projects" component={Project} />
         <Route exact path="/resume" component={Resume} />
+        </Router>
     </Switch>
 )
 export default Main;
