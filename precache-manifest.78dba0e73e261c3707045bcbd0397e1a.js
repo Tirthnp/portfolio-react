3c6895a45646c181683cc67cc67f34072fbc2a1a
Tirth Patel <tirthnp@live.com>
2019-07-08 20:07:56 -0230

    Updates

---
+++ precache-manifest.78dba0e73e261c3707045bcbd0397e1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01ef181e771b0364e34b91526a6e1dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b85f51fc943c26de49e0",
+    "url": "/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "b2b1faf7680c55e59613",
+    "url": "/static/css/main.cfbdc2dc.chunk.css"
+  },
+  {
+    "revision": "b85f51fc943c26de49e0",
+    "url": "/static/js/2.499aafb9.chunk.js"
+  },
+  {
+    "revision": "b2b1faf7680c55e59613",
+    "url": "/static/js/main.fc3d35b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dafa1e9ffd814045a271dbc7fb5e6aae",
+    "url": "/static/media/Tirth.dafa1e9f.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-react/precache-manifest.531d19da1d4c8258f0f6ab37364cf3be.js"
+  "/precache-manifest.78dba0e73e261c3707045bcbd0397e1a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
