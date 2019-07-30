9298bdeb0ec7766c6deb582c09d8d75c57edd4b8
Tirth Patel <tirthnp@live.com>
2019-07-30 18:01:22 -0230

    Updates

--- precache-manifest.5e725c74f5b5319e521b11544d0acc66.js
+++ precache-manifest.5e725c74f5b5319e521b11544d0acc66.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "103b2aaaed8344c185068928230c10e9",
+    "revision": "89be1a7ddea7f12595961d15f7871be7",
     "url": "/portfolio-react/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-react/static/css/2.04aae6c4.chunk.css"
   },
   {
-    "revision": "95f96bbedbee730443c8",
+    "revision": "b71ab63bf25bea7fd74d",
     "url": "/portfolio-react/static/css/main.cfbdc2dc.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-react/static/js/2.499aafb9.chunk.js"
   },
   {
-    "revision": "95f96bbedbee730443c8",
-    "url": "/portfolio-react/static/js/main.6acbd5e9.chunk.js"
+    "revision": "b71ab63bf25bea7fd74d",
+    "url": "/portfolio-react/static/js/main.7f27ad31.chunk.js"
   },
   {
     "revision": "11528ddda4154ce1e32b",
