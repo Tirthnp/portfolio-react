e425305823177f06e677636a11f841dd7a3830bb
Tirth Patel <tirthnp@live.com>
2019-09-24 23:11:13 -0230

    bull

--- src/App.js
+++ src/App.js
@@ -13,16 +13,16 @@ function App() {
         <Header className="header-color" title={<Link style={{textDecoration:'none',color:'white'}} to="/">MyPortfolio</Link>} scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
-                {/* <Link to="/aboutme">About Me</Link> */}
-                {/* <Link to="/projects">Projects</Link> */}
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title={<Link style={{textDecoration:'none',color:'black'}} to="/">MyPortfolio</Link>}>
             <Navigation>
                 <Link to="/resume">Resume</Link>
-                {/* <Link to="/aboutme">About Me</Link> */}
-                {/* <Link to="/projects">Projects</Link> */}
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Drawer>
--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch,Route,Router} from 'react-router-dom';
+import {Switch,Route} from 'react-router-dom';
 import Landing from './landingpage';
 import About from './aboutme';
 import Contact from './contact';
@@ -8,14 +8,23 @@ import Resume from './resume';
 
 
 const Main =()=>(
+<<<<<<< HEAD
     
         <Router basename={process.env.PUBLIC_URL}>
+=======
+    <Switch>
+        
+>>>>>>> parent of f82e5e0... changes
         <Route exact path="/" component={Landing} />
         <Route path="/aboutme" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="/projects" component={Project} />
         <Route exact path="/resume" component={Resume} />
+<<<<<<< HEAD
         </Router>
     
+=======
+    </Switch>
+>>>>>>> parent of f82e5e0... changes
 )
 export default Main;
